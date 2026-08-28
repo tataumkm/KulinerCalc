f5a96f6d5286bc46c55e8cbb8ad7b9732b09e4c0
Tata Umkm <tataumkm.id@gmail.com>
2026-08-28 18:59:20 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const DATA_BACKEND = "gas";
+export const DATA_BACKEND = "local";
 export const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxqAc0fiOngj5OBhOahvlKeA9dSKZWHT4TyScaF9sZ9s_i5DMajupEG6nDtCEyDqt8MtA/exec";
 export const ROUND_STEP = 500;
